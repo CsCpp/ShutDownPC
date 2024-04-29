41f22ed6713a07232ef870d3c89c8e40c3c5338b
cscppkotlin@gmail.com <cscppkotlin@gmail.com>
2024-04-29 09:40:09 +0300

    Добавьте файлы проекта.

---
+++ ShutDownPC/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ShutDownPC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "shutdovn.exe";
+            p.StartInfo.Arguments = "/s /t 0";
+            p.Start();
+        }
+    }
+}
